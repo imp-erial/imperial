062f890b2c5937fae97f70b992d7857c1ff8b095
Sapphire Becker <github@logicplace.com>
2020-05-15 21:45:39 -0700

    #1: exceptions needed to implement Value

--- imperial/exceptions.py
+++ imperial/exceptions.py
@@ -21,3 +21,18 @@ class ImperialKeyError(ImperialError):
 	Raised when a non-existent key was requested.
 	"""
 	pass
+
+class ImperialTypeError(ImperialError):
+	"""
+	Like a TypeError but caused by the Imperial system.
+	"""
+	def __init__(self, value, expects):
+		self.value = value
+		self.expects = expects
+		super().__init__(value, expects)
+
+class ImperialValueError(ImperialError):
+	"""
+	Like a ValueError but caused by the Imperial system.
+	"""
+	pass
